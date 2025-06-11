c1d2ed26f34c855006e78552bc78eb1a5a577148
zer0Hour-afk <fremy0031@gmail.com>
2025-06-11 15:47:32 +0800

    Perhitungan

--- src/latihan3/rumus.java
+++ src/latihan3/rumus.java
@@ -7,7 +7,24 @@ package latihan3;
 
 
 public class rumus {
-    
     //Muhammad Baihaqqi, //2310010160, //4DTIBJB
+    int sisi;
+    int panjang;
+    int lebar;
+    int hasil;
     
+    public rumus(int sisi, int panjang, int lebar) {
+        this.sisi = sisi;
+        this.panjang = panjang;
+        this.lebar = lebar;
+        this.hasil = 0; 
+    }
+
+    public void hitungLuas() {
+        hasil = panjang * lebar;
+    }
+    
+    public void tampilkanHasil() {
+        System.out.println("Hasil perhitungan: " + hasil);
+    }
 }
